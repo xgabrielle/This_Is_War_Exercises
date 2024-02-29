3341606f54fb636b934b765d1b4639e1b3445b34
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-02-29 21:07:33 +0100

    added to string

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -6,6 +6,10 @@ class my_class
     int age;
     string name;
 public:
+    my_class()
+    {
+        name = "someone's name";
+    }
     my_class(const string& animalType)
     {
        name = animalType;
@@ -18,7 +22,8 @@ public:
 };
 int main()
 {
-  
+    my_class myClass1;
+    cout << myClass1.print()<<endl;
     my_class myClass("cat");
     cout<< myClass.print();
     return 0;
