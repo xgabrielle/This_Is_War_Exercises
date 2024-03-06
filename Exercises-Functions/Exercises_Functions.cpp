6b28a6e46543615b5c3fbd780d2d6616ca44acf7
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-03-06 20:19:48 +0100

    2nd function exercise
    Write a program that asks a name say hello. Use your own function, that recives a string of characters (name) and prints on screen the hello message. (Doesn't returns anything- void type)

--- Exercises-Functions/Exercises_Functions.cpp
+++ Exercises-Functions/Exercises_Functions.cpp
@@ -2,7 +2,31 @@
 #include<iostream>
 using namespace std;
 
+// Write a program that asks a name say hello.
+// Use your own function, that recives a string of
+// characters (name) and prints on screen the hello message.
+// (Doesn't returns anything- void type)
+
+void SayHello(char name[])
+{
+    int length{};
+    while(name[length]!='\0'&&length<=10){length++;}
+    for (int i=0; i<length;i++){}
+    cout<<"Hello "<<name<<endl;
+}
+
+int main()
+{
+    char name[10];
+    cout<< "What's your name? ";
+    cin>>name;
+    SayHello(name);
+}
+
+//------------------------------------------------------------------------------
+
 // Write a program that calculates 6^5. Declare your own function to do this.
+/*
 int Pow(int base, int up)
 {
     int result=1;
@@ -21,3 +45,4 @@ int main()
     cout<<"Result = "<< result<<endl;
     
 }
+*/
