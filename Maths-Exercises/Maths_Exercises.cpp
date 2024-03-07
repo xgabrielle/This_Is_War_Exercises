0a8f76a46ee14ae6cee2db2972b1d4be1898ba8e
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-03-07 14:51:17 +0100

    New project for maths follow along

---
+++ Maths-Exercises/Maths_Exercises.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Vector
+{
+public:
+    float x,y;
+};
+class Point
+{
+public:
+    Point AddVector(Vector v);
+    float x,y;
+};
+Point Point::AddVector(Vector v)
+{
+    
+}
