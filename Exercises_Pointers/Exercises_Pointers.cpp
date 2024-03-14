4d9bcc88a74966cb7382804bb177441322b644cc
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-03-14 23:13:24 +0100

    first exercise done

--- Exercises_Pointers/Exercises_Pointers.cpp
+++ Exercises_Pointers/Exercises_Pointers.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
+/*
+ *Write a program that asks the user to enter integers as inputs
+ *to be stored in the variables 'a' and 'b' respectively.
+ *There are also two integer pointers named ptrA and ptrB.
+ *Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them.
+*/
+
+int main()
 {
+    int a{};
+    int b{};
+    int *ptrA{};
+    int *ptrB{};
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"Enter another number: ";
+    cin>>b;
+
+    ptrA = &a;
+    ptrB = &b;
+    
+    cout<<"ptrA = "<<*ptrA<<" and ptrB = "<<*ptrB;
+    
+
     
     return 0;
 }
